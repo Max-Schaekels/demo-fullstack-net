6576c5d88ca47905416f72fcd57b8073148a8b7b
Maximilien Schaekels <m.schaekels@stag.technofuturtic.education>
2025-05-30 11:40:39 +0200

    mise en place get all

--- src/app/components/contact-api/contact-api.component.ts
+++ src/app/components/contact-api/contact-api.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ContactApiService } from '../../tools/service/contact-api.service';
+import { log } from 'console';
+import { Post } from '../../models/post.models';
 
 @Component({
   selector: 'app-contact-api',
@@ -7,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrl: './contact-api.component.scss'
 })
 export class ContactApiComponent {
+
+
+  constructor(private _contactApi :ContactApiService) {  }
+
+ messageErreur : string = "";
+
+ posts : Post[] = [];
+
+ etatModif : boolean = false;
+
+ postModifie! : Post;
+
+  ngOnInit() : void {
+    this._contactApi.RecupererPosts()
+      .subscribe({
+        next : (data) => {
+          this.posts = data.slice(0,10);
+          
+        },
+        error : (error) =>{
+          this.messageErreur = error.message;
+          
+        },
+        complete : () => {
+          console.log("Récupération des posts ok !!!!");
+          
+        }
+      })
+    
+  } 
 
 }
